74fd0c69f0ac454f4812c1f51cf8cc47a2a11aa3
pawelsiemieniuk <pawel.siemieniuk11@gmail.com>
2026-03-14 19:12:49 +0100

    Poprawiono skrypty i ich strukturę

--- src/js/constants.js
+++ src/js/constants.js
@@ -16,9 +16,11 @@ const nbpListApiUrl   = 'https://api.nbp.pl/api/exchangerates/tables';
 const nbpApiTable     = ['A', 'B'];
 const nbpApiFormat    = '?format=json';
 
-// countries.js
 const countryApiUrl         = 'https://restcountries.com/v3.1/name';
 const countriesApiUrl       = 'https://restcountries.com/v3.1/all';
 const countryByCodeApiUrl   = 'https://restcountries.com/v3.1/alpha';
 const withThose             = '?fields=';
-// ---
+
+function isDOM(Obj) {
+  return Obj instanceof Element;
+}
--- src/js/countries.js
+++ src/js/countries.js
@@ -1,38 +1,101 @@
-// const countryApiUrl = 'https://restcountries.com/v3.1/all';
-// const withThose     = '?fields=';
-// const fields        = 'cca3,flag,name,currencies'
-//
 
-fields = 'cca3,flag,name,currencies'
+async function populateWithCountries(elementsArray, childType, getFullName = false) {
+  let fields = 'cca3,flag,name,currencies,translations'
 
-fetch(countriesApiUrl + withThose + fields)
-.then(response => {
-  if (!response.ok) {
-    throw new Error('Network response was not ok');
-  }
-  return response.json();
-})
-.then(responseData => {
-  const originList = calcForm.origin;
-  const targetList = calcForm.target;
+  return await fetch(countriesApiUrl + withThose + fields)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(responseData => {
+    const countries = responseData;
+    for(let country of countries) {
+      let name = country.cca3;
+      let visibleName = name;
+      if(getFullName)
+        visibleName = country.translations.pol.common; //country.name.common;
+      let flag = country.flag;
 
-  const countries = responseData;
-  for(let country of countries) {
-    let name = country.cca3;
-    let fullName = country.name.common;
-    let flag = country.flag;
+      let child = document.createElement(childType);
+      child.value = name;
+      child.innerText = flag + ' ' + visibleName;
 
-    let option = document.createElement('option');
-    option.value = name;
-    option.innerText = flag + ' ' + name;
+      if(elementsArray.constructor === Array){
+        for(let element of elementsArray) {
+          let clonedChild = child.cloneNode(true);
+          element.appendChild(clonedChild);
+        }
+      } else if(isDOM(elementsArray)) {
+        elementsArray.appendChild(child);
+      } else {
+        console.warn('Warning:', 'Argument elementsArray is not an array of elements nor an element');
+      }
+    }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+}
 
-    let originOption = option;
-    let targetOption = option.cloneNode(true);
+function insertCountryCurrency(nameElement, symbolElement, codeElement, countryCode) {
+  let fields = 'currencies'
 
-    originList.appendChild(originOption);
-    targetList.appendChild(targetOption);
-  }
-})
-.catch(error => {
-  console.error('Error:', error);
-});
+  fetch(countryByCodeApiUrl + '/' + countryCode + withThose + fields)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(responseData => {
+    if(responseData === undefined) {
+      console.warn('Invalid or missing country code!');
+    } else if(Object.keys(responseData.currencies).length === 0) {
+      console.info('No currencies found for this country');
+    } else {
+      const country = responseData;
+      let currencyCode   = Object.keys(country.currencies)[0];
+      let currencyName = country.currencies[currencyCode].name;
+      let currencySymbol = country.currencies[currencyCode].symbol;
+
+      nameElement.value   = currencyName;
+      symbolElement.value = currencySymbol;
+      codeElement.value   = currencyCode;
+    }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+}
+
+async function fillCurrencyList(currencyList) {
+  let fields = 'cca3,currencies';
+
+  fetch(countriesApiUrl + withThose + fields)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(responseData => {
+    const countries = responseData;
+    for(let country of countries) {
+      let name = country.cca3;
+      let currencyCode = Object.keys(country.currencies)[0];
+
+      if(currencyCode === undefined){
+        console.info("No currency for", name);
+        continue
+      }
+      let currencySymbol = country.currencies[currencyCode].symbol;
+
+      currencyList[name] = {'code': currencyCode, 'symbol': currencySymbol};
+    }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+}
