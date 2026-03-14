16b350669829e2863d25f68544e341e2ba4f4010
pawelsiemieniuk <pawel.siemieniuk11@gmail.com>
2026-03-14 13:22:22 +0100

    Dodano wstępną paginacje listy krajów

---
+++ src/js/constants.js
@@ -0,0 +1,24 @@
+const resultPrecision = 2;
+
+const exchangeDirection = {
+  FROM: true,
+  INTO: false
+}
+
+const countriesPerPage = 10;
+
+const EXPENSECOUNT = 'ecount';
+const EXPENSESUM   = 'esum';
+const EXPENSEBASE  = 'exp-';
+
+const nbpApiUrl       = 'https://api.nbp.pl/api/exchangerates/rates';
+const nbpListApiUrl   = 'https://api.nbp.pl/api/exchangerates/tables';
+const nbpApiTable     = ['A', 'B'];
+const nbpApiFormat    = '?format=json';
+
+// countries.js
+const countryApiUrl         = 'https://restcountries.com/v3.1/name';
+const countriesApiUrl       = 'https://restcountries.com/v3.1/all';
+const countryByCodeApiUrl   = 'https://restcountries.com/v3.1/alpha';
+const withThose             = '?fields=';
+// ---
