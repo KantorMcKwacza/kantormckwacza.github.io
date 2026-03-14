ecfe32b241b52d212f19b38d9409dc68bc2d6204
pawelsiemieniuk <pawel.siemieniuk11@gmail.com>
2026-03-14 15:39:07 +0100

    Dodano plik ze stałymi

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,9 +1,5 @@
 const resultPrecision = 2;
 
-/**
- * Kierunek wymiany waluty z PLN lub na PLN.
- * @type {boolean}
- */
 const exchangeDirection = {
   FROM: true,
   INTO: false
@@ -12,31 +8,14 @@ const exchangeDirection = {
 const countriesPerPage = 10;
 const pagesDisplayed   = 5;
 
-/**
- * Id zapisanej liczby wydatków w `localStorage`
- * @type {string}
- */
 const EXPENSECOUNT = 'ecount';
-/**
- * Id zapisanej sumy wydatków w `localStorage`
- * @type {string}
- */
 const EXPENSESUM   = 'esum';
-/**
- * Id zapisanego wydatku w `localStorage`
- *
- * Schemat id wydatku: `'exp-{numer wydatku}'`
- * @example
- * 'exp-1':'...'
- * 'exp-2':'...'
- * @type {string}
- */
 const EXPENSEBASE  = 'exp-';
 
 const nbpApiUrl       = 'https://api.nbp.pl/api/exchangerates/rates';
 const nbpListApiUrl   = 'https://api.nbp.pl/api/exchangerates/tables';
 const nbpApiTable     = ['A', 'B'];
-const nbpApiFormat    = '?format=json';
+const nbpApiFormat = '?format=json';
 
 const countryApiUrl         = 'https://restcountries.com/v3.1/name';
 const countriesApiUrl       = 'https://restcountries.com/v3.1/all';
