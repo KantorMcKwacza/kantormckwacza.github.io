1b7c13dd1c6f71c891a104ab8b5d1733401351f8
pawelsiemieniuk <pawel.siemieniuk11@gmail.com>
2026-03-22 19:11:20 +0100

    - Dodano funkcjonalność przycisków zmiany strony na następną i poprzednią  - Dodano wypełnianie listy stron w zależności od liczby krajów  - Dodano wyświetlanie numerów najbliższych stron w zależności od aktualnej strony

--- src/js/country_list.js
+++ src/js/country_list.js
@@ -1,7 +1,6 @@
 
 const countryList = document.getElementById('country-list');
-
-
+const pageList    = document.getElementById('page-list');
 
 let currentPage = 1;
 
@@ -11,7 +10,9 @@ paginateList();
 
 async function paginateList() {
   await populateWithCountries(countryList, 'li', true);
+  createPagesList();
 
+  displayPageList(currentPage);
   displayPage(currentPage);
 }
 
@@ -35,11 +36,132 @@ function getParameters() {
   }
 }
 
+function nextPage() {
+  switchPageRelative(1);
+}
+function prevPage() {
+  switchPageRelative(-1);
+}
+
+function switchPageRelative(pageShift) {
+  if(isNaN(pageShift)) {
+    console.warn("Warning:", "Invalid page shift!");
+    return;
+  }
+
+  let firstPage = 1;
+  let lastPage  = pageList.getElementsByTagName('li').length - 2;
+
+  if(currentPage + pageShift >= lastPage)
+    currentPage = lastPage;
+  else if(currentPage + pageShift <= firstPage)
+    currentPage = firstPage;
+  else
+    currentPage += pageShift;
+
+  let urlString          = window.location.href;
+  let urlParamSplit      = urlString.split('?');
+  let baseUrl            = urlParamSplit[0];
+  let paramString        = urlParamSplit[1];
+  let queryString        = new URLSearchParams(paramString);
+  let alteredParamString = "";
+
+  for(let pair of queryString.entries()) {
+    let key   = pair[0];
+    let value = pair[1];
+
+    switch(key) {
+      case 'page':
+        value = currentPage;
+        break;
+      default:
+        break;
+    }
+    if(alteredParamString === "")
+      alteredParamString = key + '=' + value;
+    else
+      alteredParamString = alteredParamString + '&' + key + '=' + value;
+  }
+
+  window.location.href = baseUrl + '?' + alteredParamString;
+}
+
+
+function createPagesList() {
+  const countriesNumber = countryList.getElementsByTagName('li').length;
+
+  let pagesNumber = countriesNumber / countriesPerPage;
+  if(pagesNumber !== Math.trunc(pagesNumber)) {
+    pagesNumber += 1;
+  }
+
+  const nextPage = pageList.removeChild(pageList.lastElementChild);
+
+  for(let p = 1; p <= pagesNumber; p++) {
+    let page = createPage(p);
+    if(page === -1) break;
+    pageList.appendChild(page);
+  }
+  pageList.appendChild(nextPage);
+}
+
+function createPage(pageNumber) {
+  if(isNaN(pageNumber)) {
+    console.warn("Warning:", "pageNumber is not a number!");
+    return -1;
+  }
+  const li   = document.createElement('li');
+  const a    = document.createElement('a');
+  const span = document.createElement('span');
+
+  span.class     = "visuallyhidden";
+  span.innerText = "strona ";
+
+  a.appendChild(span);
+
+  a.href      = "?page=" + pageNumber;
+  a.innerText = pageNumber;
+
+  li.appendChild(a);
+
+  return li;
+}
+
+function displayPageList(page) {
+  const pageElements = Array.from(pageList.getElementsByTagName('li'));
+  const lastPage  = pageElements.length - 1;
+
+  const pageShift = Math.trunc(pagesDisplayed / 2);
+
+  let startIndex = page - pageShift;
+  let endIndex   = page + pageShift;
+
+  if(startIndex < 1) {
+    endIndex   = (endIndex - startIndex) + 1;
+    startIndex = 1;
+  }
+  if(endIndex >= lastPage) {
+    let pagesAbove = endIndex - lastPage;
+    startIndex = (startIndex - pagesAbove) - 1;
+    endIndex = lastPage;
+  }
+
+  pageElements.forEach((pageElement, index) => {
+    if(index === 0 || index === pageElements.length - 1) {
+      pageElement.style.display = 'block';
+    } else if (index >= startIndex && index <= endIndex) {
+      pageElement.style.display = 'block';
+    } else {
+      pageElement.style.display = 'none';
+    }
+  });
+}
+
 function displayPage(page) {
   const countryElements = Array.from(countryList.getElementsByTagName('li'));
-  console.log(countryElements);
   const startIndex = (page - 1) * countriesPerPage;
   const endIndex = startIndex + countriesPerPage;
+
   countryElements.forEach((countryElement, index) => {
     if (index >= startIndex && index < endIndex) {
       countryElement.style.display = 'block';
